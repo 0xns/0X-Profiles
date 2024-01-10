a4084e7375d45efa0f1c10aef5f21bd86323b0f4
Hi0x <hi@hi0x.net>
2024-01-10 13:57:35 +0200

    Update Firebase.ts

--- src/lib/Firebase.ts
+++ src/lib/Firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
     apiKey: process.env.APIKEY,
-    authDomain:"projectfriendz-45b49.firebaseapp.com",
-    projectId: "projectfriendz-45b49",
-    storageBucket: "projectfriendz-45b49.appspot.com",
-    messagingSenderId: "186662584426",
-    appId: "1:186662584426:web:b37a6002f57c2af7578a13",
-    measurementId:"G-ZCMDL02FYD"
+    authDomain:"x-profiles-d9562.firebaseapp.com",
+    projectId: "x-profiles-d9562",
+    storageBucket: "x-profiles-d9562.appspot.com",
+    messagingSenderId: "154018326017",
+    appId: "1:154018326017:web:b6f7d5ad062071cb650200",
+    measurementId:"G-D2RCC93N17"
     };
     const app = initializeApp(firebaseConfig);
     const storage = getStorage(app);
